144dc4b0b034f3cc4308388132138f65d1dfcd74
Jesse MacFadyen <purplecabbage@gmail.com>
2017-09-25 17:10:48 -0700

    working oauth implementation + sample code added to readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # phonegap-plugin-oauth2
+
 Simple plugin to do OAuth2 authentication within PhoneGap/Cordova apps
+
+### Usage
+
+    document.addEventListener('deviceready', function() {
+        var endUrl = "https://phonegap.com/authCallback";
+        var startUrl = "http://github.com/login/oauth/authorize/";
+        startUrl += "?client_id=89855411f5d2ab238ad2";
+        startUrl += "&redirect_uri=" + endUrl;
+        // scope
+        // state
+        // allow_signup
+
+        PG_OAuth2.authenticate(function(url){
+            setTimeout(function() {
+                alert("Success " + url);
+            },0)
+
+        },
+        function(err) {
+            alert("error " + err);
+        },
+        startUrl,endUrl);
+    });
+
+
--- www/pg-oauth2.js
+++ www/pg-oauth2.js
@@ -6,17 +6,23 @@ var exec = cordova.require('cordova/exec'),
 
 var PG_OAuth2 = {
     authenticate: function(successCallback, errorCallback, startURL, endURL) {
-        setTimeout(function(){
-            if(successCallback) {
-                successCallback("PG_OAuth2:authenticate NotImplemented");
+        // TODO: verify args
+        var browser = cordova.InAppBrowser.open(startURL, '_blank', 'location=yes');
+        browser.addEventListener('loadstart', function(evt){
+            console.log('evt.url = ' + evt.url);
+            if(evt.url.indexOf(endURL) == 0) {
+                successCallback(evt.url);
+                browser.close();
             }
-            else if(errorCallback){
-                errorCallback("PG_OAuth2:authenticate NotImplemented");
-            }
-            else {
-                throw(new Error("PG_OAuth2:authenticate NotImplemented"));
-            }
-        },10);
+        });
+
+        //browser.addEventListener('loadstop', loadStopCallBack);
+
+        browser.addEventListener('loaderror', function(err) {
+            console.log("error " + err)
+            errorCallback(err);
+        });
+
     }
 };
 
